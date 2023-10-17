cf5d3b58c1eab9882c16137bf1a78d4f85f669ba
SoraInkling72 <CherubimKen.Fonacier72@spsmail.cuny.edu>
2023-10-16 22:47:38 -0400

    Trying to comprehend process

--- pig.py
+++ pig.py
@@ -1,3 +1,16 @@
+import random
+
+class Turn:
+    def __init__(self, roll, total):
+        self.roll = random.randint(1, 6)
+        self.total = sum([random.radint(1,6)] >= 100)
+
+    def roll(self):
+        if ([random.radint(1,6)] = 1):
+            pass
+
+
+
 
 if __name__ == "__main__":
     pass
