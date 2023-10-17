d637e9acd1b96180b1bd198d6a335871a25a3f37
SoraInkling72 <CherubimKen.Fonacier72@spsmail.cuny.edu>
2023-10-16 23:26:15 -0400

    Trying to comprehend process, let's review at another date

--- pig.py
+++ pig.py
@@ -1,16 +1,32 @@
 import random
 
-class Turn:
-    def __init__(self, roll, total):
-        self.roll = random.randint(1, 6)
-        self.total = sum([random.radint(1,6)] >= 100)
+class Pig:
+    def __init__(self, roll, winner):
+        self.roll = [random.randint(1, 6) , 'hold']
+        self.winner = sum(random.radint(1,6) >= 100)
 
-    def roll(self):
-        if ([random.radint(1,6)] = 1):
+    def Player1Turn(self, roll):
+        if self.roll == 1 or 'hold':
             pass
+        if self.roll == 2-6:
+            self.winner
+    def Player2Turn(self, roll):
+        if self.roll == 1 or 'hold':
+            pass
+        if self.roll == 2-6:
+            self.winner
 
+    def winner(self, winner):
+        if Player1Turn() >= 100 <= Player2Turn():
+            print ('Player 1 Wins')
+        if Player2Turn >= 100 <= Player1Turn():
+            print ('Player 2 Wins')
 
+Winner = Pig(roll, winner)
 
+Winner.winner()
+
+exit(Pig)
 
 if __name__ == "__main__":
     pass
